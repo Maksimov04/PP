77128f9956610a3b790196c13bfdf44d5ce51870
panda <Pandas1987bag@mail.ru>
2024-08-29 22:21:48 +0300

    HHH

--- src/main/java/com/example/pp/PpApplication.java
+++ src/main/java/com/example/pp/PpApplication.java
@@ -2,8 +2,12 @@ package com.example.pp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+
 public class PpApplication {
 
     public static void main(String[] args) {
